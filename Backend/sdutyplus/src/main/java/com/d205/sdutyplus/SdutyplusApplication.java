d5f254db66692c7dbed642d6fd35ae252dce9b93
qotlgus123 <qotlgus123@naver.com>
2022-10-28 17:48:12 +0900

    feat: 개발환경세팅 | sdutyplus 세팅 [S07P31D205-13]

---
+++ Backend/sdutyplus/src/main/java/com/d205/sdutyplus/SdutyplusApplication.java
@@ -0,0 +1,13 @@
+package com.d205.sdutyplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdutyplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdutyplusApplication.class, args);
+	}
+
+}
