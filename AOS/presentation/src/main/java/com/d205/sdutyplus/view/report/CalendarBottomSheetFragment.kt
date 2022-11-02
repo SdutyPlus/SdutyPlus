a035cd896996cd1b57e754a10f3880e74e645831
DESKTOP-KVCQHCD\multicampus <wkdrns3918@naver.com>
2022-11-02 14:51:47 +0900

    feat : 리포트 | add report calendar [S07P31D205-136]

--- AOS/presentation/src/main/java/com/d205/sdutyplus/view/report/CalendarBottomSheetFragment.kt
+++ AOS/presentation/src/main/java/com/d205/sdutyplus/view/report/CalendarBottomSheetFragment.kt
@@ -1,23 +1,160 @@
 package com.d205.sdutyplus.view.report
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.Dialog
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.d205.sdutyplus.R
+import com.d205.sdutyplus.databinding.Example1CalendarDayBinding
+import com.d205.sdutyplus.databinding.FragmentCalendarBottomSheetBinding
+import com.d205.sdutyplus.uitls.displayText
+import com.d205.sdutyplus.uitls.setTextColorRes
+import com.d205.sdutyplus.uitls.showToast
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.daysOfWeek
+import com.kizitonwose.calendar.view.CalendarView
+import com.kizitonwose.calendar.view.MonthDayBinder
+import com.kizitonwose.calendar.view.ViewContainer
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.YearMonth
 
-class CalendarBottomSheetFragment : Fragment() {
+@RequiresApi(Build.VERSION_CODES.O)
+class CalendarBottomSheetFragment : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentCalendarBottomSheetBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val monthCalendarView: CalendarView get() = binding.exOneCalendar
+
+    private val selectedDates = mutableSetOf<LocalDate>()
+    private val today = LocalDate.now()
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.setOnShowListener {  dialogInterface ->
+            val bottomSheetDialog = dialogInterface as BottomSheetDialog
+            setupRatio(bottomSheetDialog)
+        }
+        return dialog
+    }
+
+    private fun setupRatio(bottomSheetDialog: BottomSheetDialog){
+        val bottomSheet = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as View
+        val behavior = BottomSheetBehavior.from(bottomSheet)
+        val layoutParams = bottomSheet.layoutParams
+        layoutParams.height = getBottomSheetDialogDefaultHeight()
+        bottomSheet.layoutParams = layoutParams
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+    private fun getBottomSheetDialogDefaultHeight(): Int {
+        return getWindowHeight() * 75 / 100
+    }
+
+    private fun getWindowHeight(): Int {
+        val displayMetrics = DisplayMetrics()
+        (context as Activity?)!!.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.heightPixels
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_calendar_bottom_sheet, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCalendarBottomSheetBinding.bind(view)
+        val daysOfWeek = daysOfWeek()
+
+        val currentMonth = YearMonth.now()
+        val startMonth = currentMonth.minusMonths(100)
+        val endMonth = currentMonth.plusMonths(100)
+        setupMonthCalendar(startMonth, endMonth, currentMonth, daysOfWeek)
+    }
+
+    private fun setupMonthCalendar(
+        startMonth: YearMonth,
+        endMonth: YearMonth,
+        currentMonth: YearMonth,
+        daysOfWeek: List<DayOfWeek>,
+    ) {
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            lateinit var day: CalendarDay
+            val textView = Example1CalendarDayBinding.bind(view).exOneDayText
+
+            init {
+                view.setOnClickListener {
+                    if (day.position == DayPosition.MonthDate) {
+                        dateClicked(date = day.date)
+                    }
+                }
+            }
+        }
+        monthCalendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+
+            @RequiresApi(Build.VERSION_CODES.O)
+            override fun bind(container: DayViewContainer, data: CalendarDay) {
+                container.day = data
+                bindDate(data.date, container.textView, data.position == DayPosition.MonthDate)
+            }
+        }
+        monthCalendarView.monthScrollListener = { updateTitle() }
+        monthCalendarView.setup(startMonth, endMonth, daysOfWeek.first())
+        monthCalendarView.scrollToMonth(currentMonth)
+    }
+
+    private fun dateClicked(date: LocalDate) {
+        context?.showToast("$date")
+        Log.d("TAG", "dateClicked: ${date}")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun bindDate(date: LocalDate, textView: TextView, isSelectable: Boolean) {
+        textView.text = date.dayOfMonth.toString()
+        if (isSelectable) {
+            when {
+                selectedDates.contains(date) -> {
+                    textView.setTextColorRes(R.color.example_1_bg)
+                    textView.setBackgroundResource(R.drawable.bg_calendar_selected)
+                }
+                today == date -> {
+                    textView.setTextColorRes(R.color.example_1_white)
+                    textView.setBackgroundResource(R.drawable.bg_calendar_today)
+                }
+                else -> {
+                    textView.setTextColorRes(R.color.example_1_white)
+                    textView.background = null
+                }
+            }
+        } else {
+            textView.setTextColorRes(R.color.example_1_white_light)
+            textView.background = null
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("SetTextI18n")
+    private fun updateTitle() {
+        val month = monthCalendarView.findFirstVisibleMonth()?.yearMonth ?: return
+        binding.exOneYearText.text = month.year.toString()
+        binding.exOneMonthText.text = month.month.displayText(short = false)
     }
 
 }
--- AOS/presentation/src/main/java/com/d205/sdutyplus/view/report/ReportFragment.kt
+++ AOS/presentation/src/main/java/com/d205/sdutyplus/view/report/ReportFragment.kt
@@ -1,14 +1,17 @@
 package com.d205.sdutyplus.view.report
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import com.d205.sdutyplus.R
 import com.d205.sdutyplus.base.BaseFragment
 import com.d205.sdutyplus.databinding.FragmentReportBinding
 
+@RequiresApi(Build.VERSION_CODES.O)
 class ReportFragment : BaseFragment<FragmentReportBinding>(R.layout.fragment_report) {
 
     override fun initOnViewCreated() {
@@ -16,10 +19,11 @@ class ReportFragment : BaseFragment<FragmentReportBinding>(R.layout.fragment_rep
         initClickListener()
     }
 
+
     private fun initClickListener() {
         binding.ivCalendarCall.setOnClickListener {
             val dialog = CalendarBottomSheetFragment()
-            //dialog.show(supportFragmentManager, "exampleBo")
+            dialog.show(parentFragmentManager, "BottomSheet")
         }
     }
 
