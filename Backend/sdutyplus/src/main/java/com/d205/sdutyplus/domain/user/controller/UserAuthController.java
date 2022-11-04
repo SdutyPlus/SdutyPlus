0c14d4379eb0c092c2f89479d1eead5b372b9182
qotlgus123 <qotlgus123@naver.com>
2022-11-04 16:49:39 +0900

    fix: 회원관리 | 소셜로그인 token값 조회 롤백 [S07P31D205-42]

--- Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/user/controller/UserAuthController.java
+++ Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/user/controller/UserAuthController.java
@@ -34,7 +34,6 @@ public class UserAuthController {
     @ApiOperation(value="네이버 로그인")
     @PostMapping("/naver/login")
     public ResponseEntity<?> naverLogin(@RequestBody String token){
-        System.out.println(token);
         Map<String, Object> userInfo = userAuthService.getNaverUserInfo(token);
         if(userInfo!=null) {
             String email = userInfo.get("email").toString();
@@ -51,7 +50,6 @@ public class UserAuthController {
     @ApiOperation(value="카카오 로그인")
     @PostMapping("/kakao/login")
     public ResponseEntity<?> kakaoLogin(@RequestBody String token){
-        System.out.println(token);
         Map<String, Object> userInfo = userAuthService.getKakaoUserInfo(token);
         if(userInfo!=null) {
             String email = userInfo.get("email").toString();
