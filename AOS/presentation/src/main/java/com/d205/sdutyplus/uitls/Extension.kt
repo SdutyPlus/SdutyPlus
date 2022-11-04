edc019b5d758807af66fec17673eb24fe0a7aae0
woway2488 <woway2488@naver.com>
2022-11-04 16:51:01 +0900

    feat : 개발 환경 세팅 | 디바이스 사이즈 구하는 static 함수 추가 [S07P31D205-210]

--- AOS/presentation/src/main/java/com/d205/sdutyplus/uitls/Extension.kt
+++ AOS/presentation/src/main/java/com/d205/sdutyplus/uitls/Extension.kt
@@ -1,5 +1,9 @@
 package com.d205.sdutyplus.uitls
 
+import android.graphics.Insets
+import android.graphics.Point
+import android.view.WindowInsets
+import android.view.WindowManager
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -10,4 +14,28 @@ fun getTodayDate(): Date {
 fun convertTimeDateToString(date: Date, format: String = "yyyy년 M월 d일"): String {
     val simpleDateFormat = SimpleDateFormat(format, Locale("ko", "KR"))
     return simpleDateFormat.format(date)
+}
+
+fun WindowManager.currentWindowMetricsPointCompat() : Point {
+    // R(30) 이상
+    return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+        val windowInsets = currentWindowMetrics.windowInsets
+        var insets: Insets = windowInsets.getInsets(WindowInsets.Type.navigationBars())
+        windowInsets.displayCutout?.run {
+            insets = Insets.max(
+                insets,
+                Insets.of(safeInsetLeft, safeInsetTop, safeInsetRight, safeInsetBottom)
+            )
+        }
+        val insetsWidth = insets.right + insets.left
+        val insetsHeight = insets.top + insets.bottom
+        Point(
+            currentWindowMetrics.bounds.width() - insetsWidth,
+            currentWindowMetrics.bounds.height() - insetsHeight
+        )
+    } else {
+        Point().apply {
+            defaultDisplay.getSize(this)
+        }
+    }
 }
