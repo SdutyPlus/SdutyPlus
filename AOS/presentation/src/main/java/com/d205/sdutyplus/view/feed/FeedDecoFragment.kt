e70271d8c57d5b380a3f367490b75a1b34991992
nhee0410 <nhee0410@pusan.ac.kr>
2022-11-04 16:08:39 +0900

    feat: 공부인증 | 게시물 등록에서 촬영하거나 갤러리에서 이미지 선택 [S07P31D205-128]

--- AOS/presentation/src/main/java/com/d205/sdutyplus/view/feed/FeedDecoFragment.kt
+++ AOS/presentation/src/main/java/com/d205/sdutyplus/view/feed/FeedDecoFragment.kt
@@ -1,17 +1,48 @@
 package com.d205.sdutyplus.view.feed
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.d205.sdutyplus.R
 import com.d205.sdutyplus.base.BaseFragment
 import com.d205.sdutyplus.databinding.FragmentFeedDecoBinding
+import com.d205.sdutyplus.uitls.PROFILE
+import com.d205.sdutyplus.view.common.CropImageActivity
 
 class FeedDecoFragment : BaseFragment<FragmentFeedDecoBinding>(R.layout.fragment_feed_deco) {
 
-    override fun initOnViewCreated() {
+    private var imageUrl: String = ""
 
+    override fun initOnViewCreated() {
+        initView()
+    }
+
+    private fun initView() {
+        launchImageCrop()
+    }
+
+    private fun launchImageCrop() {
+        val intent = Intent(requireContext(), CropImageActivity::class.java)
+        intent.putExtra("flag", PROFILE)
+        getImageResultLauncher.launch(intent)
+    }
+
+    private val getImageResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == Activity.RESULT_OK){
+            imageUrl = it.data!!.getStringExtra("uri")!!
+            setUriIntoImgPreview()
+        }
+        else{
+            findNavController().popBackStack() // 전에 보니까 이것만 Utils로 따로 빼서 쓰시던데..
+        }
+    }
+
+    private fun setUriIntoImgPreview() {
+        Glide.with(requireContext())
+            .load(Uri.parse(imageUrl).path)
+            .into(binding.imgPreview)
     }
 }
