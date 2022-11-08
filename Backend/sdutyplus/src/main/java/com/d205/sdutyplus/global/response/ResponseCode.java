29d9ba7c1e15b230ada370369b19e63a88288326
DESKTOP-KVCQHCD\multicampus <dpfls3433@gmail.com>
2022-11-08 09:30:11 +0900

    feat : 응답 코드 형식 | feed의 response code 업데이트 [S07P31D205-299]

--- Backend/sdutyplus/src/main/java/com/d205/sdutyplus/global/response/ResponseCode.java
+++ Backend/sdutyplus/src/main/java/com/d205/sdutyplus/global/response/ResponseCode.java
@@ -29,8 +29,17 @@ public enum ResponseCode {
     GET_LOCALDATETIME_SUCCESS(200, "TI001", "서버 날짜, 시간 조회가 성공하였습니다."),
 
     //Feed
-    CREATE_FEED_SUCCESS(200, "F001", "피드가 등록되었습니다."),
+    CREATE_FEED_SUCCESS(200, "F001", "게시글이 등록되었습니다."),
     GET_ALL_FEED_SUCCESS(200, "F002", "전체 게시글 조회가 성공하였습니다."),
+    UPDATE_FEED_SUCCESS(200, "F003", "게시글이 수정되었습니다."),
+    GET_MY_FEED_SUCCESS(200, "F004", "자신이 작성한 게시글 조회가 성공하였습니다."),
+    GET_SCRAP_FEED_SUCCESS(200, "F005", "자신이 스크랩한 게시글 조회가 성공하였습니다."),
+    GET_JOB_FILTER_FEED_SUCCESS(200, "F006", "직업으로 필터링한 게시글 조회가 성공하였습니다."),
+    DELETE_FEED_SUCCESS(200, "F007", "게시글이 삭제되었습니다."),
+    UPDATE_GOOD_FEED_SUCCESS(200, "F008", "해당 게시글에 좋아요가 등록되었습니다."),
+    UPDATE_UNGOOD_FEED_SUCCESS(200, "F009", "해당 게시글에 좋아요 취소하였습니다."),
+    UPDATE_SCRAP_FEED_SUCCESS(200, "F010", "해당 게시글을 스크랩하였습니다."),
+    UPDATE_UNSCRAP_FEED_SUCCESS(200, "F011", "해당 게시글을 스크랩 취소하였습니다."),
 
     // Warn
     WARN_SUCCESS(200, "W001", "신고 완료."),
