5416321af20d654383aa6df52334ff0d542fca7a
qotlgus123 <qotlgus123@naver.com>
2022-11-08 14:24:28 +0900

    feat: Exception | 프로필 Exception 처리 완료 [S07P31D205-298]

--- Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/user/service/UserService.java
+++ Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/user/service/UserService.java
@@ -1,18 +1,21 @@
 package com.d205.sdutyplus.domain.user.service;
 
 import com.d205.sdutyplus.domain.user.dto.UserDto;
-import com.d205.sdutyplus.domain.user.dto.UserLoginDto;
 import com.d205.sdutyplus.domain.user.dto.UserProfileDto;
 import com.d205.sdutyplus.domain.user.dto.UserRegDto;
-import com.d205.sdutyplus.domain.user.entity.SocialType;
 import com.d205.sdutyplus.domain.user.entity.User;
+import com.d205.sdutyplus.domain.user.exception.NicknameAlreadyExistException;
 import com.d205.sdutyplus.domain.user.repository.UserRepository;
+import com.d205.sdutyplus.global.error.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
+import static com.d205.sdutyplus.global.error.ErrorCode.USER_NOT_FOUND;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -20,29 +23,35 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public UserDto userRegData(Long userSeq, UserRegDto userRegDto) {
-        User user = userRepository.findBySeq(userSeq).get();
+    public void userRegData(Long userSeq, UserRegDto userRegDto) {
+        final User user = userRepository.findBySeq(userSeq).get();
 
-        user.setNickname(userRegDto.getNickname());
-        user.setImgUrl(userRegDto.getImgUrl());
-        user.setJob(userRegDto.getJob());
+        if (userRepository.existsByNickname(userRegDto.getNickname())
+            && !user.getNickname().equals(userRegDto.getNickname())) {
+            throw new NicknameAlreadyExistException();
+        }
 
-        UserDto userDto = new UserDto(user);
-
-        return userDto;
+        updateUserData(user, userRegDto);
     }
 
     @Transactional
     public boolean checkNicknameDuplicate(String nickname) {
-
-        return userRepository.existsByNickname(nickname);
+        return !userRepository.existsByNickname(nickname);
     }
 
     @Transactional
     public UserProfileDto getUserProfile(Long userSeq){
-        User user = userRepository.findBySeq(userSeq).get();
-        UserProfileDto userProfileDto = new UserProfileDto(user);
+        final User user = userRepository.findBySeq(userSeq)
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND));
 
-        return userProfileDto;
+        final UserProfileDto result = new UserProfileDto(user);
+
+        return result;
+    }
+
+    private void updateUserData(User user, UserRegDto userRegDto){
+        user.setNickname(userRegDto.getNickname());
+        user.setImgUrl(userRegDto.getImgUrl());
+        user.setJob(userRegDto.getJob());
     }
 }
