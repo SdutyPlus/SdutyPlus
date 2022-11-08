ca279595ae7f1356599fa025da656039b5a0e51d
DESKTOP-KVCQHCD\multicampus <dpfls3433@gmail.com>
2022-11-08 15:28:08 +0900

    feat : 테스크 API | 서브 테스크 상세 조회 [S07P31D205-263]

--- Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/task/repository/querydsl/TaskRepositoryQuerydsl.java
+++ Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/task/repository/querydsl/TaskRepositoryQuerydsl.java
@@ -4,7 +4,9 @@ import com.d205.sdutyplus.domain.task.dto.TaskResponseDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepositoryQuerydsl {
     List<TaskResponseDto> findTaskByStartTime(Long userSeq, LocalDateTime start, LocalDateTime end);
+    Optional<TaskResponseDto> findTaskBySeq(Long taskSeq);
 }
--- Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/task/repository/querydsl/TaskRepositoryQuerydslImpl.java
+++ Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/task/repository/querydsl/TaskRepositoryQuerydslImpl.java
@@ -4,6 +4,7 @@ import com.d205.sdutyplus.domain.task.dto.QSubTaskResponseDto;
 import com.d205.sdutyplus.domain.task.dto.SubTaskResponseDto;
 import com.d205.sdutyplus.domain.task.dto.TaskResponseDto;
 import com.d205.sdutyplus.domain.task.entity.Task;
+import com.d205.sdutyplus.global.error.exception.EntityNotFoundException;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static com.d205.sdutyplus.domain.task.entity.QSubTask.subTask;
@@ -35,6 +37,24 @@ public class TaskRepositoryQuerydslImpl implements TaskRepositoryQuerydsl{
         return transform.entrySet().stream()
                 .map(entry -> new TaskResponseDto(entry.getKey().getSeq(), entry.getKey().getStartTime(), entry.getKey().getEndTime(), entry.getKey().getContent(), entry.getValue()))
                 .collect(Collectors.toList());
+
+    }
+
+    @Override
+    public Optional<TaskResponseDto> findTaskBySeq(Long taskSeq) {
+        Map<Task, List<SubTaskResponseDto>> transform = queryFactory
+                .selectFrom(task)
+                .leftJoin(task.subTasks, subTask)
+                .where(task.seq.eq(taskSeq))
+                .transform(groupBy(task).as(list(new QSubTaskResponseDto(subTask.seq, subTask.content))));
+
+//        System.out.println(transform.entrySet().stream()
+//                .map(entry -> new TaskResponseDto(entry.getKey().getSeq(), entry.getKey().getStartTime(), entry.getKey().getEndTime(), entry.getKey().getContent(), entry.getValue()))
+//                .findFirst()
+//        );
+        return (Optional<TaskResponseDto>) transform.entrySet().stream()
+                .map(entry -> new TaskResponseDto(entry.getKey().getSeq(), entry.getKey().getStartTime(), entry.getKey().getEndTime(), entry.getKey().getContent(), entry.getValue()))
+                .findFirst();
 
     }
 
