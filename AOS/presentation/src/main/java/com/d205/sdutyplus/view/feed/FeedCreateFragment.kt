47f9f7f9dc6add58ad9a50b57d6c54a772d7eeee
nhee0410 <nhee0410@pusan.ac.kr>
2022-11-09 14:43:59 +0900

    feat: 공부인증 | 게시물 작성 시, 사용자에게 길이 제한 피드백 [S07P31D205-230]

--- AOS/presentation/src/main/java/com/d205/sdutyplus/view/feed/FeedCreateFragment.kt
+++ AOS/presentation/src/main/java/com/d205/sdutyplus/view/feed/FeedCreateFragment.kt
@@ -1,6 +1,9 @@
 package com.d205.sdutyplus.view.feed
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +20,8 @@ class FeedCreateFragment : BaseFragment<FragmentFeedCreateBinding>(R.layout.frag
     }
 
     private fun initView() {
+        limitEditTextLength()
+
         binding.apply {
             ivBack.setOnClickListener {
                 findNavController().popBackStack()
@@ -24,6 +29,29 @@ class FeedCreateFragment : BaseFragment<FragmentFeedCreateBinding>(R.layout.frag
             tvAddImg.setOnClickListener {
                 moveToFeedDecoFragment()
             }
+        }
+    }
+
+    private fun limitEditTextLength() {
+        binding.apply {
+            etWrite.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(p0: Editable?) {}
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    tvWordLength.text = "${etWrite.length()} / 200"
+                    if (etWrite.length() > 200) {
+                        //requireContext().showToast("최대 200자까지 입력 가능합니다.")
+                        etWrite.setTextColor(Color.RED)
+                        tvWordLength.setTextColor(Color.RED)
+                    }
+                    else {
+                        etWrite.setTextColor(Color.BLACK)
+                        tvWordLength.setTextColor(Color.BLACK)
+                    }
+                }
+            })
         }
     }
 
