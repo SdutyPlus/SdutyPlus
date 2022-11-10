ef4cd54b58baaf73bd1374fac73c0faea002e3bb
woway2488 <woway2488@naver.com>
2022-11-10 10:49:33 +0900

    fix : 타이머 | current Task Dto 2 로 변경, suspend 지움 [S07P31D205-66]

--- AOS/data/src/main/java/com/d205/data/repository/timer/remote/TimerRemoteDataSource.kt
+++ AOS/data/src/main/java/com/d205/data/repository/timer/remote/TimerRemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.d205.data.repository.timer.remote
 
 import com.d205.domain.model.timer.CurrentTaskDto
+import com.d205.domain.model.timer.CurrentTaskDto2
 import kotlinx.coroutines.flow.Flow
 
 interface TimerRemoteDataSource {
@@ -8,5 +9,5 @@ interface TimerRemoteDataSource {
 
     suspend fun getTodayTotalStudyTime(): String
 
-    suspend fun addTask(currentTaskDto: CurrentTaskDto): Flow<Boolean>
+    fun addTask(currentTaskDto: CurrentTaskDto2): Flow<Boolean>
 }
