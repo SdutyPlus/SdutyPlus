86d568e888ff6c659349353260ae09822b4bbd98
DESKTOP-KVCQHCD\multicampus <toy9910@naver.com>
2022-11-15 17:19:29 +0900

    feat: 마이페이지 | feed 이미지 안보이는 에러 수정  [S07P31D205-351]

--- AOS/data/src/main/java/com/d205/data/model/feed/FeedResponse.kt
+++ AOS/data/src/main/java/com/d205/data/model/feed/FeedResponse.kt
@@ -2,8 +2,11 @@ package com.d205.data.model.feed
 
 import com.d205.domain.model.feed.Writer
 
-data class HomeFeedResponse(
+data class FeedResponse(
     val seq: Int = 0,
-    val writer: Writer ,
-    val imgUrl: String
+    val writer: Writer,
+    var imgUrl: String = "",
+    var content: String = "",
+    var feedLikesCount: Int = 0,
+    var scrapCount: Int = 0
 )
--- AOS/data/src/main/java/com/d205/data/model/mypage/MyPageResponse.kt
+++ AOS/data/src/main/java/com/d205/data/model/mypage/MyPageResponse.kt
@@ -1,12 +1,5 @@
 package com.d205.data.model.mypage
 
+import com.d205.domain.model.feed.Writer
 
-data class MyFeedResponse(
-    val seq: Int = 0,
-    var writerSeq: Int = 0,
-    var nickname: String = "",
-    var profileImgUrl: String = "",
-    var feedImgUrl: String = "",
-    var content: String = "",
-    var scrapCnt: Int = 0
-)
+
