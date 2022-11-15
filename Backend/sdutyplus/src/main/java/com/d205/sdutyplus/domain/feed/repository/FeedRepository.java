2f191aa3962a3caa33c413571630af22a1150569
DESKTOP-KVCQHCD\multicampus <dpfls3433@gmail.com>
2022-11-15 14:34:17 +0900

    refactor:  피드 service repository, dsl 코드 간결화 [S07P31D205-99]

--- Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/feed/repository/FeedRepository.java
+++ Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/feed/repository/FeedRepository.java
@@ -1,9 +1,10 @@
 package com.d205.sdutyplus.domain.feed.repository;
 
 import com.d205.sdutyplus.domain.feed.entity.Feed;
+import com.d205.sdutyplus.domain.feed.repository.querydsl.FeedRepositoryQuerydsl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FeedRepository extends JpaRepository<Feed, Long> {
+public interface FeedRepository extends JpaRepository<Feed, Long>, FeedRepositoryQuerydsl {
     void deleteAllByWriterSeq(Long userSeq);
 
 }
