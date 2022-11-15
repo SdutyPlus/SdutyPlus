bd21b7ed18cb50f11151fcee0fcc8f78e70945bc
DESKTOP-KVCQHCD\multicampus <dpfls3433@gmail.com>
2022-11-15 15:29:31 +0900

    fix: 피드 삭제 | 연관 관계 삭제 후, 수행 [S07P31D205-278]

--- Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/warn/repository/WarnFeedRepository.java
+++ Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/warn/repository/WarnFeedRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface WarnFeedRepository extends JpaRepository<WarnFeed, Long> {
     boolean existsByUserAndFeed(User user, Feed feed);
     void deleteAllByUserSeq(Long userSeq);
+    void deleteAllByFeedSeq(Long feedSeq);
 }
