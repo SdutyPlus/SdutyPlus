71758ca3d8967446111ffd4d4379f5856ba7d32b
woway2488 <woway2488@naver.com>
2022-11-15 12:53:03 +0900

    c

--- AOS/presentation/src/main/java/com/d205/sdutyplus/view/feed/FeedFragment.kt
+++ AOS/presentation/src/main/java/com/d205/sdutyplus/view/feed/FeedFragment.kt
@@ -5,15 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.d205.domain.model.mypage.Feed
+import com.d205.domain.model.user.User
 import com.d205.sdutyplus.R
 import com.d205.sdutyplus.base.BaseFragment
 import com.d205.sdutyplus.databinding.FragmentFeedBinding
+import kotlinx.coroutines.flow.collectLatest
 
 class FeedFragment : BaseFragment<FragmentFeedBinding>(R.layout.fragment_feed) {
 
+    private lateinit var feedAdapter: FeedAdapter
+    private lateinit var user: User
+    private val feedViewModel: FeedViewModel by activityViewModels()
+
     override fun initOnViewCreated() {
+
+        user = mainViewModel.user.value!!
+
+        initAdapter()
         initView()
+        initViewModel()
+    }
+
+    private fun initAdapter() {
+        feedAdapter = FeedAdapter(requireActivity())
+        feedAdapter.apply {
+            onClickStoryListener = object : FeedAdapter.OnClickStoryListener{
+                override fun onClick(feed: Feed) {
+                    //feed click 시 이벤트
+                }
+            }
+        }
     }
 
     private fun initView() {
@@ -21,6 +45,14 @@ class FeedFragment : BaseFragment<FragmentFeedBinding>(R.layout.fragment_feed) {
         displayBottomNav(true)
         binding.ivCreateFeed.setOnClickListener {
             moveToFeedCreateFragment()
+        }
+    }
+
+    private fun initViewModel() {
+        this@FeedFragment.feedViewModel.apply {
+            pagingFeedList.collectLatest {
+                feedAdapter.submitData(this@FeedFragment.lifecycle, it)
+            }
         }
     }
 
