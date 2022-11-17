893bea62777bcb4d1d5d8af99f61095ab2447bf9
DESKTOP-KVCQHCD\multicampus <wkdrns3918@naver.com>
2022-11-17 16:15:26 +0900

    style : report ui 수정

--- AOS/presentation/src/main/java/com/d205/sdutyplus/view/report/graph/graphFragment.kt
+++ AOS/presentation/src/main/java/com/d205/sdutyplus/view/report/graph/graphFragment.kt
@@ -2,12 +2,6 @@ package com.d205.sdutyplus.view.report.graph
 
 import android.annotation.SuppressLint
 import android.graphics.Color
-import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import com.d205.sdutyplus.R
@@ -15,10 +9,11 @@ import com.d205.sdutyplus.base.BaseFragment
 import com.d205.sdutyplus.databinding.FragmentGraphBinding
 import com.d205.sdutyplus.view.report.ReportViewModel
 import com.github.mikephil.charting.animation.Easing
-import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -59,36 +54,69 @@ class graphFragment : BaseFragment<FragmentGraphBinding>(R.layout.fragment_graph
 
             val pieDataSet = PieDataSet(data(), "공부시간")
 
-            val pieData = PieData(pieDataSet)
-
-
             pieDataSet.apply {
                 colors = colorPalette
                 sliceSpace = 3f
                 selectionShift = 5f
+
+
+                setValueLinePart1OffsetPercentage(80f)
+                setValueLinePart1Length(0.2f)
+                setValueLinePart2Length(0.4f)
+                setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE)
             }
 
+            val pieData = PieData(pieDataSet)
+
             pieData.apply {
-                setValueTextSize(16F)
+                setValueFormatter(PercentFormatter())
+                setValueTextSize(14f)
                 setValueTextColor(Color.BLACK)
-                val des = Description()
-                des.text = "시간별 공부시간"
             }
 
             binding.piechart.apply {
+
                 setUsePercentValues(true)
+
+                setExtraOffsets(5f, 10f, 5f, 5f)
+                setDragDecelerationFrictionCoef(0.95f)
+
+                setExtraOffsets(20f, 0f, 20f, 0f)
+                setEntryLabelTextSize(16F)
+
+                setDrawHoleEnabled(true)
+                setHoleColor(Color.WHITE)
+
+                setTransparentCircleColor(Color.WHITE)
+                setTransparentCircleAlpha(110)
+
+                setRotationAngle(0f)
+                // enable rotation of the chart by touch
+                setRotationEnabled(true)
+                setHighlightPerTapEnabled(true)
+
+
+
                 // 부가 설명
                 description.isEnabled = false
-                setExtraOffsets(0f, 5f, 0f, 5f)
+
                 isDrawHoleEnabled = true
                 setHoleColor(Color.WHITE)
                 transparentCircleRadius = 61f
-                animateY(1000, Easing.EaseInOutCubic)
+                animateY(1400, Easing.EaseInOutCubic)
                 centerText = "${studyTime}시간 이상 \n공부하였습니다."
                 setCenterTextSize(20F)
                 data = pieData
                 invalidate()
             }
+
+            // chart.spin(2000, 0, 360);
+            val l: Legend = binding.piechart.getLegend()
+            l.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+            l.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+            l.orientation = Legend.LegendOrientation.VERTICAL
+            l.setDrawInside(false)
+            l.isEnabled = false
         }
     }
 
@@ -99,7 +127,7 @@ class graphFragment : BaseFragment<FragmentGraphBinding>(R.layout.fragment_graph
     private fun data(): ArrayList<PieEntry> {
         val datavalue: ArrayList<PieEntry> = ArrayList()
 
-        datavalue.add(PieEntry(graphList[0].toFloat(), "0~2시간"))
+        datavalue.add(PieEntry(graphList[0].toFloat(), "2시간 미만"))
         datavalue.add(PieEntry(graphList[1].toFloat(), "2~4시간"))
         datavalue.add(PieEntry(graphList[2].toFloat(), "4~6시간"))
         datavalue.add(PieEntry(graphList[3].toFloat(), "6~8시간"))
