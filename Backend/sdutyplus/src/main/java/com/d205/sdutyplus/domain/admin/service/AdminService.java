2ea38ae977417d5d146f5c2c6f664823b70f4624
DESKTOP-KVCQHCD\multicampus <dpfls3433@gmail.com>
2022-11-20 17:37:13 +0900

    refactor: 클린 코드 | admin 트랜잭션 처리 수정 [S07P31D205-365]

--- Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/admin/service/AdminService.java
+++ Backend/sdutyplus/src/main/java/com/d205/sdutyplus/domain/admin/service/AdminService.java
@@ -23,19 +23,17 @@ public class AdminService {
     private final UserRepository userRepository;
     private final AuthUtils authUtils;
 
-    @Transactional
     public PagingResultDto getWarnFeed(Pageable pageable){
         final Long userSeq = authUtils.getLoginUserSeq();
         final Page<FeedResponseDto> warnFeeds = feedRepository.findAllWarnFeedPage(userSeq, pageable);
-        final PagingResultDto pagingResultDto = new PagingResultDto<FeedResponseDto>(pageable.getPageNumber(), warnFeeds.getTotalPages() - 1, warnFeeds.getContent());
+        final PagingResultDto<FeedResponseDto> pagingResultDto = new PagingResultDto(pageable.getPageNumber(), warnFeeds.getTotalPages() - 1, warnFeeds.getContent());
 
         return pagingResultDto;
     }
 
-    @Transactional
     public PagingResultDto getWarnUser(Pageable pageable){
         final Page<WarnUserDto> warnUsers = userRepository.findAllWarnUserPage(pageable);
-        final PagingResultDto pagingResultDto = new PagingResultDto<WarnUserDto>(pageable.getPageNumber(), warnUsers.getTotalPages() - 1, warnUsers.getContent());
+        final PagingResultDto<WarnUserDto> pagingResultDto = new PagingResultDto(pageable.getPageNumber(), warnUsers.getTotalPages() - 1, warnUsers.getContent());
 
         return pagingResultDto;
     }
