4900521b65ff563239c86290b771ff705345173c
서재형 <woway2488@naver.com>
2023-03-10 12:11:25 +0900

    rename : Common | Utils, Extension 파일 용도에 맞게 함수들 위치 변경

--- AOS/presentation/src/main/java/com/d205/sdutyplus/utils/Utils.kt
+++ AOS/presentation/src/main/java/com/d205/sdutyplus/utils/Utils.kt
@@ -5,9 +5,28 @@ import android.content.Context
 import android.graphics.Point
 import android.view.WindowManager
 import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
 
-fun Context.showToast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+fun getTodayDate(): Date {
+    return Date(System.currentTimeMillis())
+}
+
+fun convertTimeDateToString(date: Date, format: String = "yyyy년 M월 d일"): String {
+    val simpleDateFormat = SimpleDateFormat(format, Locale("ko", "KR"))
+    return simpleDateFormat.format(date)
+}
+
+fun convertTimeStringToDate(str: String, format: String): Date {
+    val simpleDateFormat = SimpleDateFormat(format, Locale("ko", "KR"))
+    return simpleDateFormat.parse(str)
+}
+
+fun getTodayDateString(): String {
+    val simpleDateFormat = SimpleDateFormat("yy.MM.dd", Locale("ko", "KR"))
+    val currentDate = simpleDateFormat.format(Date())
+
+    return currentDate
 }
 
 fun getDeviceSize(activity: Activity): Point {
