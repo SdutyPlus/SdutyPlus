3d80697f5582faf3bdd40cb29e35e2f485c17dc5
서재형 <woway2488@naver.com>
2023-03-16 18:25:56 +0900

    feat: Login | TestLogin 경고 Diaglog class 생성

---
+++ AOS/presentation/src/main/java/com/d205/sdutyplus/view/login/dialog/TestLoginDialog.kt
@@ -0,0 +1,18 @@
+package com.d205.sdutyplus.view.login.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class TestLoginDialog : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
