05e500ad886d81aa379cdb66a3e686e9eb4c43e5
pmi4202@naver.com <pmi4202@naver.com>
2023-03-17 17:23:33 +0900

    test: 테스트 계정 | 테스트용 계정 생성 테스트 코드 변경

--- Backend/sdutyplus/src/test/java/com/d205/sdutyplus/user/UserTestLoginTest.java
+++ Backend/sdutyplus/src/test/java/com/d205/sdutyplus/user/UserTestLoginTest.java
@@ -1,6 +1,7 @@
 package com.d205.sdutyplus.user;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,12 +19,13 @@ public class UserTestLoginTest {
     private JPAQueryFactory queryFactory;
 
     @Test
-    public void makeIDTest(){
-        //1. socialType이 SDUTY인 것 중 최근 값 불러오기, 없으면 1번 유저로 새로 만들기
+    @DisplayName("테스트용 계정 생성 테스트")
+    public void makeTestIDTest(){
         List<String> testUsers = getLastUserID();
-        testUsers.add("test2@sduty.com");
-        //2. parsing해서 새로운 아이디 생성하기 (ID값 = test[번호]@sduty.com 으로 변경하기, socialType = sduty)
+        String testAccount = "test2@sduty.com";
         String newUserEmail;
+        testUsers.add(testAccount);
+
         if(testUsers.size()==0){
             newUserEmail = "test1@sduty.com";
         }
